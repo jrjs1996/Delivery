c7cac9c68f89c27540489b87bd78a0a77d0ba0f4
jrjs1996 <jrjs1996@hotmail.com>
2019-05-31 16:16:05 -0600

    Added Side Menu

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,12 @@
 import React, { Component } from 'react';
 import axios from 'axios';
-import logo from './logo.svg';
 import './App.css';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+
+import logo from './logo.svg';
+import SideMenu from './Components/SideMenu';
 
 class App extends Component {
   constructor(props) {
@@ -34,6 +39,14 @@ class App extends Component {
     const { apiResponse } = this.state;
     return (
       <div className="App">
+        <AppBar position="static">
+          <Toolbar>
+            <Typography variant="title" color="inherit">
+              asdfasdf asdfasdf asdfasdf
+            </Typography>
+          </Toolbar>
+        </AppBar>
+        <SideMenu />
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p className="App-intro">
