0d426643158011b609a44908f75b4194cfd9ae77
jrjs1996 <jrjs1996@hotmail.com>
2019-06-02 17:08:07 -0600

    Moved customer components to their own folder.

--- client/src/Components/Customers/Customers.js
+++ client/src/Components/Customers/Customers.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import { fetchCustomers } from '../actions/customerActions';
-import AddCustomerForm from './AddCustomerForm';
+import { fetchCustomers } from '../../actions/customerActions';
+import AddCustomerForm from '../AddCustomerForm';
 
 class Customers extends Component {
   componentWillMount() {
