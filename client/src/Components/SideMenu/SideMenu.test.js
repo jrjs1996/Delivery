1054cd5a2d7cd4d65a6343c0d468358bba3a0d0d
jrjs1996 <jrjs1996@hotmail.com>
2019-06-18 20:13:07 -0600

    Created snippets for tests and docs

---
+++ client/src/Components/SideMenu/SideMenu.test.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { create } from 'react-test-renderer';
+import { render, cleanup, fireEvent } from '@testing-library/react';
+import 'jest-dom/extend-expect';
+import SideMenu.test from './SideMenu.test
+
+
+describe('SideMenu.test', () => {
+  beforeEach(() => {
+
+  });
+
+  afterEach(() => {
+
+  });
+});
+
