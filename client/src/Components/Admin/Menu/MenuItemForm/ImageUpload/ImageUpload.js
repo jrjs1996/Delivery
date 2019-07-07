e688735a5200df5644fe0431296f1add833550fc
jrjs1996 <jrjs1996@hotmail.com>
2019-07-06 18:44:52 -0600

    Image alert isn't shown. Image is uploaded when dropped or selected.

--- client/src/Components/Admin/Menu/MenuItemForm/ImageUpload/ImageUpload.js
+++ client/src/Components/Admin/Menu/MenuItemForm/ImageUpload/ImageUpload.js
@@ -1,15 +1,19 @@
-import React, { useState } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import { DropzoneArea } from 'material-ui-dropzone';
 import { Button } from '@material-ui/core';
-
+import '../../AdminMenu/AdminMenu.css';
 
 export default function ImageUpload({ id, uploadImage }) {
-  const [files, setFiles] = useState([]);
   return (
     <div>
-      <DropzoneArea onChange={setFiles} dropzoneText="Upload item image" />
-      <Button variant="contained" onClick={() => uploadImage(id, files[0])} color="primary">Upload</Button>
+      <DropzoneArea
+        dropzoneText="Upload item image"
+        filesLimit={1}
+        onChange={files => uploadImage(id, files[0])}
+        showAlerts={false}
+        showPreviewsInDropzone={false}
+      />
     </div>
   );
 }
