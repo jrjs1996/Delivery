38ef2564fee7373348c62dfb5065f46dd2435502
jrjs1996 <jrjs1996@hotmail.com>
2019-07-10 06:04:18 -0600

    Now displays error if create order failed

--- client/src/actions/orderActions.js
+++ client/src/actions/orderActions.js
@@ -1,14 +1,22 @@
 import axios from 'axios';
-import { ADD_ORDER, FETCH_ORDERS, COMPLETE_ORDER, ERROR } from './types';
+import {
+  ADD_ORDER, FETCH_ORDERS, COMPLETE_ORDER, ERROR,
+} from './types';
 import { setAuthHeader } from '../utils/token';
 
-export const fetchOrders = () => (dispatch) => {
-  axios.get('/api/orders/').then((res) => {
+export const fetchOrders = () => async (dispatch) => {
+  try {
+    const res = await axios.get('/api/orders/');
     dispatch({
       type: FETCH_ORDERS,
       payload: res.data,
     });
-  });
+  } catch (error) {
+    dispatch({
+      type: ERROR,
+      payload: 'Unable to fetch orders',
+    });
+  }
 };
 
 export const fetchOpenOrders = () => async (dispatch) => {
@@ -26,13 +34,19 @@ export const fetchOpenOrders = () => async (dispatch) => {
   }
 };
 
-export const createOrder = postData => (dispatch) => {
-  axios.post('/api/orders/', postData).then((res) => {
+export const createOrder = postData => async (dispatch) => {
+  try {
+    const res = await axios.post('/api/orders/', postData);
     dispatch({
       type: ADD_ORDER,
       payload: res.data,
     });
-  });
+  } catch (error) {
+    dispatch({
+      type: ERROR,
+      payload: 'Unable to create order',
+    });
+  }
 };
 
 export const updateOrder = (id, putData) => (dispatch) => {
